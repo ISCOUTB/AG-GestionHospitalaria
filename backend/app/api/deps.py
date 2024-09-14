4b31bfd57ddc95ad2712b5cfd2ce7730d117b9ce
dosquisd <pereznjuandiego@gmail.com>
2024-09-14 10:28:48 -0500

    Ir creando el esquema de la API poco a poco (agregar la ruta para el ingreso al sistema hasta el momento)

---
+++ backend/app/api/deps.py
@@ -0,0 +1,87 @@
+from collections.abc import Generator
+from typing import Annotated
+
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
+from pydantic import ValidationError
+from sqlalchemy.orm import Session
+
+from app.core.config import settings
+from app.core.db import SessionLocal
+from app.core.security import verify_password
+
+from app import schemas, models
+
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl=f'{settings.API_V1_STR}/login/access-token'
+)
+
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+SessionDep = Annotated[Session, Depends(get_db)]
+TokenDep = Annotated[Session, Depends(reusable_oauth2)]
+
+
+def get_current_user(db: SessionDep, token: TokenDep) -> schemas.models.UserRoles:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="No se pudieron validar las credenciales",
+        headers={"WWW-Authenticate": "bearer"}
+    )
+
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        token_data = schemas.TokenPayload(
+            number_document=payload.get("number_document"),
+            rol=payload.get("rol")
+        )
+    except (ValidationError, InvalidTokenError):
+        raise credentials_exception
+
+    # Lógica para obtener al usuario
+    pass
+
+
+CurrentUser = Annotated[schemas.models.UserRoles, Depends(get_current_user)]
+
+
+def get_superuser(current_user: CurrentUser) -> schemas.models.UserRoles | None:
+    if current_user.num_document == settings.FIRST_SUPERUSER and \
+        verify_password(settings.FIRST_SUPERUSER_PASSWORD, current_user.password):
+        return current_user
+    
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail="Usuario no autorizado")
+
+
+def get_current_admin(current_user: CurrentUser) -> schemas.models.UserRoles | None:
+    if current_user.rol != 'admin':
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Usuario no autorizado")
+    
+    return current_user
+
+
+def get_current_doctor(current_user: CurrentUser) -> schemas.models.UserRoles | None:
+    if current_user.rol != 'doctor':
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail="Usuario no autorizado")
+    
+    return current_user
+
+
+def get_current_patient(current_user: CurrentUser) -> schemas.models.UserRoles | None:
+    if current_user.rol != 'patient':
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail="Usuario no autorizado")
+    
+    return current_user
---
+++ backend/app/api/routes/login.py
@@ -0,0 +1,38 @@
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status, Body
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.api.deps import CurrentUser, SessionDep
+from app.core import security
+
+from app import schemas
+
+router = APIRouter(prefix="/login")
+
+
+@router.get("/")
+async def root() -> dict:
+    return {"detail": "root/login", "status": status.HTTP_200_OK}
+
+
+@router.post("/access-token")
+async def login_access_token(
+    db: SessionDep, form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    rol: Annotated[schemas.Roles, Body()]
+) -> schemas.Token:
+    """  """
+    user_search: schemas.UserSearch = schemas.UserSearch(
+        number_document=form_data.username,
+        rol=rol
+    )
+
+    # Se autentica el usuario y se sigue con toda la lógica...
+    pass
+
+
+async def test_token(current_user: CurrentUser) -> schemas.models.UserRoles:
+    """ Probar el token de acceso """
+    return current_user
+
