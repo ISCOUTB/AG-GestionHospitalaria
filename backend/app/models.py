3bd7a3f66cee18553aa313701d951c44a01396be
dosquisd <pereznjuandiego@gmail.com>
2024-09-14 10:24:26 -0500

    Corrección de errores en las relaciones y agregar nuevas restricciones y columnas

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,7 +9,7 @@ from app.core.db import BaseModel
 class UsersInfo(BaseModel):
     __tablename__ = 'users_info'
 
-    number_document = Column(String, primary_key=True)
+    num_document = Column(String, primary_key=True)
     type_document = Column(String, default=None, nullable=True)
     name = Column(String, default=None, nullable=True)
     surname = Column(String, default=None, nullable=True)
@@ -17,8 +17,9 @@ class UsersInfo(BaseModel):
     birthday = Column(Date, default=None, nullable=True)
     address = Column(String, default=None, nullable=True)
     phone = Column(String, default=None, nullable=True)
+    email = Column(String, default=None, nullable=True, unique=True)
 
-    user_roles = relationship('UserRoles', uselist=False, back_populates='users_info',
+    user_roles = relationship('UserRoles', uselist=True, back_populates='users_info',
                               passive_deletes=True, passive_updates=True)
 
 
@@ -26,24 +27,33 @@ class UserRoles(BaseModel):
     __tablename__ = 'user_roles'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    num_document = Column(String, ForeignKey('users_info.number_document'), nullable=False)
+    num_document = Column(String, ForeignKey('users_info.num_document'), nullable=False)
     rol = Column(String, nullable=False)
     password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True, nullable=False)
     inactivity = Column(Date, default=None, nullable=True)
 
-    users_info = relationship('UsersInfo', uselist=False, back_populates='user_roles',
+    users_info = relationship('UsersInfo', uselist=True, back_populates='user_roles',
                               passive_deletes=True, passive_updates=True)
     patient_info = relationship('PatientInfo', uselist=False, back_populates='user_roles',
                                 passive_deletes=True, passive_updates=True)
-    doctor_specialities = relationship('DoctorSpecialities', uselist=False, back_populates='user_roles',
+    doctor_specialities = relationship('DoctorSpecialities', uselist=True, back_populates='user_roles',
                                        passive_deletes=True, passive_updates=True)
-    beds_used = relationship('BedsUsed', uselist=False, back_populates='user_roles',
-                             passive_deletes=True, passive_updates=True)
-    medical_consults = relationship('MedicalConsults', uselist=False, back_populates='user_roles',
+    
+    beds_used_doctor = relationship('BedsUsed', uselist=True, foreign_keys='BedsUsed.id_doctor', back_populates='doctor',
                                     passive_deletes=True, passive_updates=True)
-    hospitalizations = relationship('Hospitalizations', uselist=False, back_populates='user_roles',
+    beds_used_patient = relationship('BedsUsed', uselist=False, foreign_keys='BedsUsed.id_patient', back_populates='patient',
+                                     passive_deletes=True, passive_updates=True)
+
+    medical_consults_patient = relationship('MedicalConsults', uselist=True, foreign_keys='MedicalConsults.id_patient', back_populates='patient',
+                                            passive_deletes=True, passive_updates=True)
+    medical_consults_doctor = relationship('MedicalConsults', uselist=True, foreign_keys='MedicalConsults.id_doctor', back_populates='doctor',
                                     passive_deletes=True, passive_updates=True)
+    
+    hospitalizations_patient = relationship('Hospitalizations', uselist=True, foreign_keys='Hospitalizations.id_patient', back_populates='patient',
+                                            passive_deletes=True, passive_updates=True)
+    hospitalizations_doctor = relationship('Hospitalizations', uselist=True, foreign_keys='Hospitalizations.id_doctor', back_populates='doctor',
+                                           passive_deletes=True, passive_updates=True)
 
 
 class PatientInfo(BaseModel):
@@ -51,7 +61,7 @@ class PatientInfo(BaseModel):
 
     patient_id = Column(Integer, ForeignKey('user_roles.id'), primary_key=True)
     num_doc_responsable = Column(String, default=None, nullable=True)
-    type_doc_responsable = Column(default=None, nullable=True)
+    type_doc_responsable = Column(String, default=None, nullable=True)
     name_responsable = Column(String, default=None, nullable=True)
     surname_responsable = Column(String, default=None, nullable=True)
     phone_responsable = Column(String, default=None, nullable=True)
@@ -68,7 +78,7 @@ class Specialities(BaseModel):
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
 
-    doctor_specialities = relationship('DoctorSpecialities', uselist=False, back_populates='specialities',
+    doctor_specialities = relationship('DoctorSpecialities', uselist=True, back_populates='specialities',
                                        passive_deletes=True, passive_updates=True)
 
 
@@ -79,9 +89,9 @@ class DoctorSpecialities(BaseModel):
     doctor_id = Column(Integer, ForeignKey('user_roles.id'), nullable=False)
     speciality_id = Column(Integer, ForeignKey('specialities.id'), nullable=False)
 
-    specialities = relationship('Specialities', uselist=False, back_populates='doctor_specialities',
+    specialities = relationship('Specialities', uselist=True, back_populates='doctor_specialities',
                                 passive_deletes=True, passive_updates=True)
-    user_roles = relationship('UserRoles', uselist=False, back_populates='doctor_specialities',
+    user_roles = relationship('UserRoles', uselist=True, back_populates='doctor_specialities',
                               passive_deletes=True, passive_updates=True)
 
 
@@ -92,7 +102,7 @@ class Beds(BaseModel):
     room = Column(String, nullable=False, unique=True)  # Esto es básicamente una llave primaria
     # Se le pueden seguir agregando otros atributos en caso de de ser necesario
 
-    beds_used = relationship('BedsUsed', uselist=False, back_populates='',
+    beds_used = relationship('BedsUsed', uselist=False, back_populates='beds',
                              passive_deletes=True, passive_updates=True)
     
 
@@ -100,14 +110,16 @@ class BedsUsed(BaseModel):
     __tablename__ = 'beds_used'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    id_bed = Column(Integer, ForeignKey('beds.id'), nullable=False)
-    id_patient = Column(Integer, ForeignKey('user_roles.id'), nullable=False)
+    id_bed = Column(Integer, ForeignKey('beds.id'), unique=True, nullable=False)
+    id_patient = Column(Integer, ForeignKey('user_roles.id'), unique=True, nullable=False)
     id_doctor = Column(Integer, ForeignKey('user_roles.id'), nullable=False)
 
     beds = relationship('Beds', uselist=False, back_populates='beds_used',
                         passive_deletes=True, passive_updates=True)
-    user_roles = relationship('UserRoles', uselist=False, back_populates='beds_used',
-                              passive_deletes=True, passive_updates=True)
+    patient = relationship('UserRoles', uselist=False, foreign_keys=[id_patient], back_populates='beds_used_patient',
+                           passive_deletes=True, passive_updates=True)
+    doctor = relationship('UserRoles', uselist=True, foreign_keys=[id_doctor], back_populates='beds_used_doctor',
+                          passive_deletes=True, passive_updates=True)
 
 
 class MedicalConsults(BaseModel):
@@ -119,8 +131,10 @@ class MedicalConsults(BaseModel):
     area = Column(String, nullable=False)
     day = Column(Date, default=datetime.date.today(), nullable=False)
 
-    user_roles = relationship('UserRoles', uselist=False, back_populates='medical_consults',
-                              passive_deletes=True, passive_updates=True)
+    patient = relationship('UserRoles', uselist=True, foreign_keys=[id_patient], back_populates='medical_consults_patient',
+                           passive_deletes=True, passive_updates=True)
+    doctor = relationship('UserRoles', uselist=True, foreign_keys=[id_doctor], back_populates='medical_consults_doctor',
+                          passive_deletes=True, passive_updates=True)
 
 
 class Hospitalizations(BaseModel):
@@ -132,5 +146,7 @@ class Hospitalizations(BaseModel):
     entry_day = Column(Date, default=datetime.date.today(), nullable=False)
     last_day = Column(Date, default=None, nullable=True)
 
-    user_roles = relationship('UserRoles', uselist=False, back_populates='hospitalizations',
-                              passive_deletes=True, passive_updates=True)
+    patient = relationship('UserRoles', uselist=True, foreign_keys=[id_patient], back_populates='hospitalizations_patient',
+                           passive_deletes=True, passive_updates=True)
+    doctor = relationship('UserRoles', uselist=True, foreign_keys=[id_doctor], back_populates='hospitalizations_doctor',
+                          passive_deletes=True, passive_updates=True)
