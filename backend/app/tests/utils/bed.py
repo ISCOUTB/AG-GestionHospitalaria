bde02f113d1c41943821e86dc9d1b2895e2ccf68
dosquisd <pereznjuandiego@gmail.com>
2024-10-19 18:01:10 -0500

    Agregar una nueva utilidad para agregar camas aleatorias

---
+++ backend/app/tests/utils/bed.py
@@ -0,0 +1,18 @@
+import random
+
+from sqlalchemy.orm import Session
+
+from app.crud import crud_bed
+from app.schemas import BedBase
+
+
+def create_random_bed(db: Session) -> BedBase:
+    room = f'random_bed{random.choice(range(100))}'
+    bed = BedBase(room=room)
+    out = crud_bed.add_bed(bed, db)
+
+    while out == 1:
+        room = f'random_bed{random.choice(range(100))}'
+        out = crud_bed.add_bed(bed, db)
+
+    return bed
