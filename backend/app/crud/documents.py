54a772e99308f4f0c2e52d446eafffc719bf4454
JuanD <dosquisd123@gmail.com>
2024-11-01 19:39:38 -0500

    Terminar la implementación de las operaciones crud

--- backend/app/crud/documents.py
+++ backend/app/crud/documents.py
@@ -1,10 +1,13 @@
 import os
+from pathlib import Path
 
 from app import schemas
 from app.core.config import settings
 
 from fastapi import UploadFile
 from fastapi.responses import FileResponse
+
+from datetime import datetime
 from typing import Literal
 
 
@@ -112,7 +115,8 @@ class CRUDDocuments:
             None: Crea la historia clínica de un paciente como un archivo txt vacío. En caso
             de ya existir, lo omite.
         """
-        pass
+        patient_path: str = f"{settings.PATIENT_DOCS_PATH}/{num_document}"
+        Path(f'{patient_path}/{settings.HISTORY_FILENAME}').touch()
 
     def update_history(self, num_document: str, history: UploadFile) -> Literal[0, 1]:
         """
@@ -131,7 +135,7 @@ class CRUDDocuments:
         """
         pass
     
-    def add_order(self, num_document: str, order: UploadFile) -> Literal[0, 1]:
+    async def add_order(self, num_document: str, order: UploadFile) -> Literal[0, 1]:
         """
         Agrega una orden médica a un determinado paciente
 
@@ -146,9 +150,21 @@ class CRUDDocuments:
             - 0: Resultado exitoso.
             - 1: Error guardando el archivo.
         """
-        pass
+        patient_path: str = f"{settings.PATIENT_DOCS_PATH}/{num_document}"
+        order_path: str = f"{patient_path}/orders"
+        filename: str = f'{order_path}/{order.filename}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+        
+        try:
+            with open(filename, 'wb') as f:
+                content = await order.read()
+                f.write(content)
+        except Exception as e:
+            print(repr(e))
+            return 1
 
-    def add_result(self, num_document: str, result: UploadFile) -> Literal[0, 1]:
+        return 0
+
+    async def add_result(self, num_document: str, result: UploadFile) -> Literal[0, 1]:
         """
         Agrega un resultado médico de un determinado paciente
 
@@ -163,9 +179,21 @@ class CRUDDocuments:
             - 0: Resultado exitoso.
             - 1: Error guardando el archivo.
         """
-        pass
+        patient_path: str = f"{settings.PATIENT_DOCS_PATH}/{num_document}"
+        result_path: str = f"{patient_path}/results"
+        filename: str = f'{result_path}/{result.filename}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+        
+        try:
+            with open(filename, 'wb') as f:
+                content = await result.read()
+                f.write(content)
+        except Exception as e:
+            print(repr(e))
+            return 1
+        
+        return 0
 
-    def delete_file(self, num_document: str, filename: str):
+    def delete_file(self, num_document: str, filename: str, order: bool = False):
         """
         Elimina un archivo médico de un determinado paciente (no incluye la historia clínica)
 
@@ -173,14 +201,32 @@ class CRUDDocuments:
             num_document (str): Número de documento del paciente al que se le quiere actualizar
             la historia clínica
             filename (str): Nombre del archivo que se desea eliminar
+            order (bool): Indica si el archivo es de la orden médica o de resultados médicos
 
         Returns:
             int: Retorna un número entero simbolizando el estado de la respuesta. Estos son los
             los posibles resultados:
             - 0: Resultado exitoso.
-            - 1: Error guardando el archivo.
+            - 1: Archivo no encontrado.
+            - 2: Error desconocido borrando el archivo.
         """
-        pass
+        patient_path: str = f"{settings.PATIENT_DOCS_PATH}/{num_document}"
+        if order:
+            order_path: str = f"{patient_path}/orders"
+            filename: str = f'{order_path}/{filename}'
+        else:
+            result_path: str = f"{patient_path}/results"
+            filename: str = f'{result_path}/{filename}'
+        
+        try:
+            os.remove(filename)
+        except FileNotFoundError:
+            return 1
+        except Exception as e:
+            print(repr(e))
+            return 2
+        
+        return 0
 
 
 crud_document = CRUDDocuments()
