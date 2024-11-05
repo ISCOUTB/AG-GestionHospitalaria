a3812f98003e18d4a704615aa210631f68fdc959
JuanD <dosquisd123@gmail.com>
2024-11-05 03:49:27 -0500

    Agregar nuevo endpoint para obtener el historial de la API

--- backend/app/api/routes/admins.py
+++ backend/app/api/routes/admins.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter
+from datetime import date, datetime
+from time import perf_counter
 
-from app.api.deps import SessionDep, Admin
+from fastapi import APIRouter, Request, status
+
+from app.api.deps import SessionDep, Admin, collection, log_request
+
+from app import schemas
 
 router = APIRouter()
 
@@ -16,3 +21,51 @@ async def get_stats(current_user: Admin, db: SessionDep) -> list[float]:
         3. Cantidad de admisiones y altas por día.
     """
     pass
+
+
+@router.get("/api-historial")
+async def get_api_historial(
+    request: Request,
+    current_user: Admin,
+    limit: int = -1,
+    start_date: date | None = None,
+    end_date: date | None = None,
+) -> list[schemas.ApiHistorial]:
+    """
+    Obtiene el historial de la API guardadas en la base de datos en mongodb. 
+    """
+    start_time = perf_counter()
+
+    if limit <= 0:
+        limit = collection.count_documents({})
+
+    date_filter = {}
+
+    if start_date is not None:
+        date_filter["$gte"] = datetime.combine(start_date, datetime.min.time())
+    if end_date is not None:
+        date_filter["$lte"] = datetime.combine(end_date, datetime.max.time())
+
+    query = {"timestamp": date_filter} if date_filter else {}    
+    documents = collection.find(query).limit(limit)
+
+    result: list[schemas.ApiHistorial] = []
+    for document in documents:
+        result.append(
+            schemas.ApiHistorial(
+                username=document["username"],
+                rol=document["rol"],
+                timestamp=document["timestamp"],
+                method=document["method"],
+                url=document["url"],
+                headers=document["headers"],
+                body=document["body"],
+                process_time_ms=document["process_time_ms"],
+                status_code=document["status_code"],
+            )
+        )
+
+    process_time = perf_counter() - start_time
+    log_data = [process_time, None, current_user.num_document, current_user.rol]
+    await log_request(request, status.HTTP_200_OK, *log_data)
+    return result
--- backend/app/schemas/api.py
+++ backend/app/schemas/api.py
@@ -1,4 +1,6 @@
+from typing import Any
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class ApiResponse(BaseModel):
@@ -10,3 +12,30 @@ class ApiResponse(BaseModel):
     """
 
     detail: str
+
+
+class ApiHistorial(BaseModel):
+    """
+    Clase base para obtener el historial de la API.
+
+    Attributes:
+        username (str): Nombre del usuario que realizó la operación.
+        rol (str): Rol del usuario que realizó la operación.
+        timestamp (datetime.datetime): Fecha y hora de la operación.
+        method (str): Método de la operación.
+        url (str): URL de la operación.
+        headers (dict): Cabeceras de la petición de la operación.
+        body (dict[str, Any] | None): Cuerpo de la petición de la operación.
+        process_time_ms (float): Tiempo de procesamiento de la operación en milisegundos.
+        status_code (int): Código de estado de la respuesta de la operación.
+    """
+
+    username: str
+    rol: str
+    timestamp: datetime
+    method: str
+    url: str
+    headers: dict[str, Any]
+    body: dict[str, Any] | None
+    process_time_ms: float
+    status_code: int
